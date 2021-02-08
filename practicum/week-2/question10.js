a5c6bba3ab23fdd21f8779e59beeba7107544ad7
BermanBai <50157088+BermanBai@users.noreply.github.com>
2021-02-08 22:19:29 +0800

    Update question10.js

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,15 +8,22 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-    var value = 0;
-     for (var i = 0; i<cookies.length;i++){
-             
-        value+=cookies[i];
-     }
-     value += extraCookies;
-     if (value%cookies.length==0){
-             return true;
-     }else{
-             return fales;
-     }
+    var max = 0;
+
+    var min = n;
+    
+    value = value.toString();
+
+    for (var i = 0; i < n.length; i++) {
+
+        if (value.charAt(i) > max){
+
+            max = value.charAt(i);
+
+        } if (value.charAt(i) <= min) {
+
+            min = value.charAt(i);
+        }
+    }
+        return max -min;
 };
