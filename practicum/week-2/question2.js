07f80c2d967f36d9af4ad506730f99327f60bb27
BermanBai <50157088+BermanBai@users.noreply.github.com>
2021-02-08 22:21:49 +0800

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,14 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
  */
 
  var canGetEqualCookies = function(cookies, extraCookies) {
-     
-    
+          var value = 0;
+          for (var i = 0; i<cookies.length;i++){
+                   value+=cookies[i];
+          }
+          value += extraCookies;
+          if (value%cookies.length==0){
+                   return true;
+          }else{
+                   return fales;
+          }
  };
