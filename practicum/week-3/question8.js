90b3d06962b2187cd79dc4078d2b3e51748b1265
BermanBai <50157088+BermanBai@users.noreply.github.com>
2021-02-22 22:09:31 +0800

    Update question8.js

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -13,5 +13,28 @@ output: 3
 */
 
 var d_integer = function(input) {
+  
+    var result = -1;
+
+    for (var i = 0; i < input.length; i++){
+
+        var nums = 1;
+
+        for (var j = 0; j <input.length; j++){
+
+            if (input[j] == input[i] && i !=j){
+
+                nums += 1;
+            } 
+        }
+        if (nums == input[i]) {
+
+            if (input[i] > esult) {
+
+                esult = input[i];
+            }
+        }
+    }
+    return result;
 
 };
