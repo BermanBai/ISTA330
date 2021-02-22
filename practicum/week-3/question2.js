3abaad95568a64a3ae1cafba13bca839cae20f59
BermanBai <50157088+BermanBai@users.noreply.github.com>
2021-02-22 21:21:25 +0800

    Update question2.js

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,6 +10,24 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
+  
+        var sum = 0;
+  
+        var currnum = 0
+        
+        for (var i = 0; i < input.length; i++) {
+         
+                currnum = currnum + input[i];
+                if (currnum > sum) {
+                 
+                        sum = currnum;
+                }
+                if (currnum < 0) {
+                 
+                        currnum = 0;
+                }
+        }
+        return sum  
      
     
  };
